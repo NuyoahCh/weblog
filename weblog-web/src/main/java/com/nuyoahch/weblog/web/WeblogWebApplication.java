ffd6431b1d8f04c6f5ec90ac36b97bb495cb1379
NuyoahCh <springoflotus@gmail.com>
2025-02-25 19:15:22 +0800

    feat : 项目初始化成功

---
+++ weblog-web/src/main/java/com/nuyoahch/weblog/web/WeblogWebApplication.java
@@ -0,0 +1,17 @@
+package com.nuyoahch.weblog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: weblog
+ * @author: NuyoahCh
+ * @create: 2025-02-25
+ * @description: Web 启动类
+ */
+@SpringBootApplication
+public class WeblogWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeblogWebApplication.class, args);
+    }
+}
